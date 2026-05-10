4b941221bc3cd33d2b66087715b277717ba7004c
Duc Ng <danielbaker06072001@gmail.com>
2026-05-10 10:31:12 -0400

    style: apply prettier to SDK + workspace yaml, ignore next-env.d.ts

--- sdks/typescript/src/index.ts
+++ sdks/typescript/src/index.ts
@@ -2,11 +2,11 @@
 // Type definitions are generated from Rust by `cargo run -p tl-codegen`.
 // See README.md in src/generated for regen instructions.
 
-export * from "./generated/CheckRequest";
-export * from "./generated/Decision";
-export * from "./generated/Verdict";
-export * from "./generated/Channel";
-export * from "./generated/Severity";
-export * from "./generated/TriggeredPolicy";
+export * from './generated/CheckRequest';
+export * from './generated/Decision';
+export * from './generated/Verdict';
+export * from './generated/Channel';
+export * from './generated/Severity';
+export * from './generated/TriggeredPolicy';
 
-export { Client } from "./client";
+export { Client } from './client';
